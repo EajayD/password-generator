6bf82c42572d5fc43928a609efc3c6952b23faa9
EajayD <edelos1524@hotmail.com>
2021-06-23 20:21:53 -0700

    added variables

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Assignment Code
+var alpha = ["abcdefghijklmnopqrstuvwxyz"];
+var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var numbers = ["0123456789"];
+var special = ["!@#$%^&*<>?-_+=:;~"];
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword();   
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;

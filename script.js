4f2f40f284e12ca0e148e184ea3f1707b16d9a8e
EajayD <edelos1524@hotmail.com>
2021-06-24 15:56:23 -0700

    added for loop,separated array manually so for loop would work

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment Code
-var alpha = ["abcdefghijklmnopqrstuvwxyz"];
-var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var numbers = ["0123456789"];
-var special = ["!@#$%^&*<>?-_+=:;~"];
+var alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var alphaUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = [0,1,2,3,4,5,6,7,8,9];
+var special = ["!","@","#","$","%","^","&","*","<",">","?","-","_","+","=",":",";","~"];
 var characters;
 var userAlpha;
 var userAlphaUpper;
@@ -92,6 +92,14 @@ function writePassword() {
         console.log(userSelection);
       }
       
+      var final = ""
+      
+      for (var i = 0; i < characters; i++) {
+        final = final + userSelection[Math.floor(Math.random() * userSelection.length)];
+        console.log(final);
+      }
+      return final;
+
       }  
   var passwordText = document.querySelector("#password");
 

d14894145760a83f83f087752e533fb08777b964
EajayD <edelos1524@hotmail.com>
2021-06-23 23:24:23 -0700

    added while loop for not choosing a criteria

--- script.js
+++ script.js
@@ -18,11 +18,18 @@ function writePassword() {
       characters = Number(prompt("Please specify length of password, Must be 8-128 characters"));
       if (isNaN(characters) || characters < 8 || characters > 128){
         alert("MUST BE 8-128 CHARACTERS ONLY");
+        return;
       }
       userAlpha = confirm("Do you want lowercase letters?");
       userAlphaUpper = confirm("Do you want uppercase letters?");
       userNumbers = confirm("Do you want numbers?");
       userSpecial = confirm("Do you want special characters?");
+
+      while (!userAlpha && !userAlphaUpper && !userNumbers && !userSpecial) {
+        alert("You must select at least one character type!");
+        generatePassword();
+      }
+
 
       }  
   var passwordText = document.querySelector("#password");

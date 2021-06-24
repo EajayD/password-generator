9da945733491b55007f1c8da663705127b8fa75e
EajayD <edelos1524@hotmail.com>
2021-06-23 22:26:05 -0700

    added more variables, added conditional statement

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ var alpha = ["abcdefghijklmnopqrstuvwxyz"];
 var alphaUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var numbers = ["0123456789"];
 var special = ["!@#$%^&*<>?-_+=:;~"];
+var characters;
+var userAlpha;
+var userAlphaUpper;
+var userNumbers;
+var userSpecial;
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();   
+    var password = generatePassword();   
+    
+      function generatePassword() {
+      characters = Number(prompt("Please specify length of password, Must be 8-128 characters"));
+      if (isNaN(characters) || characters < 8 || characters > 128){
+        alert("MUST BE 8-128 CHARACTERS ONLY");
+      }
+    }
+
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
